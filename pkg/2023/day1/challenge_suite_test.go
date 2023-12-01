2ca750c39954ef5d0c8e4de8198544eb5cc74f97
iru <irune.prado@sysdig.com>
2023-12-01 21:26:34 +0100

    2022

---
+++ pkg/2023/day1/challenge_suite_test.go
@@ -0,0 +1,13 @@
+package challenge
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func Test(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Challenge Suite")
+}
---
+++ pkg/2023/day1/challenge_test.go
@@ -0,0 +1,47 @@
+package challenge
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("challenge", func() {
+	/*
+		calibration value
+		per line - first and last digit combined -  form a number = calibration
+		sum all those numbers
+	*/
+	Context("Star1", func() {
+		When("given sample", func() {
+			It("resolves", func() {
+				Expect(Star1("star1_sample.txt")).To(Equal(142))
+			})
+		})
+		When("given input", func() {
+			It("resolves", func() {
+				Expect(Star1("star1_input.txt")).To(Equal(56042))
+			})
+		})
+	})
+
+	/*
+		calibration value
+		per line - first and last digit combined -  form a number = calibration
+		sum all those numbers
+	*/
+	Context("Star2", func() {
+		When("given sample", func() {
+			It("resolve ...", func() {
+				Expect(TryNumberWord("eightwothree")).To(Equal(8))
+				Expect(TryNumberWord("twothree")).To(Equal(2))
+				Expect(TryNumberWord("three")).To(Equal(3))
+				Expect(Star2("star2_sample.txt")).To(Equal(281))
+			})
+		})
+		When("given input", func() {
+			It("resolves", func() {
+				Expect(Star2("star1_input.txt")).To(Equal(55358))
+			})
+		})
+	})
+})
