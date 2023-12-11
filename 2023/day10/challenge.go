7a96c3a27fcd8fb8bcc72db5f8bd9d396ac94fc2
iru <irune.prado@sysdig.com>
2023-12-11 01:01:31 +0100

    day10

---
+++ 2023/day10/challenge.go
@@ -0,0 +1,314 @@
+package challenge
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"strings"
+)
+
+func FindStart(inputFileName string) *MapPoint {
+
+	inputData, err := os.ReadFile(inputFileName)
+	if err != nil {
+		fmt.Print(err)
+		return nil
+	}
+
+	for x, dataLine := range strings.Split(string(inputData), "\n") {
+		for y, data := range dataLine {
+			if string(data) == "S" {
+				return &MapPoint{x, y, "S"}
+			}
+		}
+	}
+
+	return nil
+}
+
+func GetAdjacentValidMoves(inputFileName string, currentPointX int, currentPointY int) []MapPoint {
+	data, err := os.ReadFile(inputFileName)
+	if err != nil {
+		fmt.Print(err)
+		return nil
+	}
+
+	dataRows := strings.Split(string(data), "\n")
+	currentPoint := string(dataRows[currentPointX][currentPointY])
+	up := "."
+	if currentPointX-1 >= 0 {
+		up = string(dataRows[currentPointX-1][currentPointY])
+	}
+
+	down := "."
+	if currentPointX+1 <= len(dataRows)-1 {
+		down = string(dataRows[currentPointX+1][currentPointY])
+	}
+
+	left := "."
+	if currentPointY-1 >= 0 {
+		left = string(dataRows[currentPointX][currentPointY-1])
+	}
+
+	right := "."
+	if currentPointY+1 <= len(dataRows[currentPointX])-1 {
+		right = string(dataRows[currentPointX][currentPointY+1])
+	}
+
+	var validMoves []MapPoint
+	if IsValidAdjacent(currentPoint, up, "Up") {
+		validMoves = append(validMoves, MapPoint{currentPointX - 1, currentPointY, up})
+	}
+	if IsValidAdjacent(currentPoint, down, "Down") {
+		validMoves = append(validMoves, MapPoint{currentPointX + 1, currentPointY, down})
+	}
+	if IsValidAdjacent(currentPoint, left, "Left") {
+		validMoves = append(validMoves, MapPoint{currentPointX, currentPointY - 1, left})
+	}
+	if IsValidAdjacent(currentPoint, right, "Right") {
+		validMoves = append(validMoves, MapPoint{currentPointX, currentPointY + 1, right})
+	}
+
+	return validMoves
+}
+
+type Position string
+
+type MapPoint struct {
+	x      int
+	y      int
+	symbol string
+}
+
+func IsValidAdjacent(currentPoint string, adjacentPoint string, adjacentPosition Position) bool {
+
+	switch currentPoint {
+	case "S":
+		switch adjacentPosition {
+		case "Up":
+			return adjacentPoint == "|" || adjacentPoint == "F" || adjacentPoint == "7"
+		case "Down":
+			return adjacentPoint == "|" || adjacentPoint == "J" || adjacentPoint == "L"
+		case "Right":
+			return adjacentPoint == "-" || adjacentPoint == "J" || adjacentPoint == "7"
+		case "Left":
+			return adjacentPoint == "-" || adjacentPoint == "L" || adjacentPoint == "F"
+		}
+		return false
+	case "|":
+		switch adjacentPosition {
+		case "Up":
+			return adjacentPoint == "|" || adjacentPoint == "F" || adjacentPoint == "7" || adjacentPoint == "S"
+		case "Down":
+			return adjacentPoint == "|" || adjacentPoint == "J" || adjacentPoint == "L" || adjacentPoint == "S"
+		}
+		return false
+	case "-":
+		switch adjacentPosition {
+		case "Right":
+			return adjacentPoint == "-" || adjacentPoint == "J" || adjacentPoint == "7" || adjacentPoint == "S"
+		case "Left":
+			return adjacentPoint == "-" || adjacentPoint == "L" || adjacentPoint == "F" || adjacentPoint == "S"
+		}
+		return false
+	case "L":
+		switch adjacentPosition {
+		case "Up":
+			return adjacentPoint == "|" || adjacentPoint == "F" || adjacentPoint == "7" || adjacentPoint == "S"
+		case "Right":
+			return adjacentPoint == "-" || adjacentPoint == "J" || adjacentPoint == "7" || adjacentPoint == "S"
+		}
+		return false
+	case "J":
+		switch adjacentPosition {
+		case "Up":
+			return adjacentPoint == "|" || adjacentPoint == "F" || adjacentPoint == "7" || adjacentPoint == "S"
+		case "Left":
+			return adjacentPoint == "-" || adjacentPoint == "L" || adjacentPoint == "F" || adjacentPoint == "S"
+		}
+		return false
+	case "7":
+		switch adjacentPosition {
+		case "Down":
+			return adjacentPoint == "|" || adjacentPoint == "J" || adjacentPoint == "L" || adjacentPoint == "S"
+		case "Left":
+			return adjacentPoint == "-" || adjacentPoint == "L" || adjacentPoint == "F" || adjacentPoint == "S"
+		}
+		return false
+	case "F":
+		switch adjacentPosition {
+		case "Down":
+			return adjacentPoint == "|" || adjacentPoint == "J" || adjacentPoint == "L" || adjacentPoint == "S"
+		case "Right":
+			return adjacentPoint == "-" || adjacentPoint == "J" || adjacentPoint == "7" || adjacentPoint == "S"
+		}
+		return false
+	case ".":
+		return false
+
+	}
+	return false
+}
+
+func GetLoop(inputFileName string) []MapPoint {
+	//var loops []MapPoint
+	startPoint := FindStart(inputFileName)
+	validMoves := GetAdjacentValidMoves(inputFileName, startPoint.x, startPoint.y)
+	for _, validMove := range validMoves {
+		possibleLoop := ContinueDirection(inputFileName, []MapPoint{*startPoint, validMove})
+
+		if possibleLoop == nil {
+			continue
+		}
+
+		if IsStartingPoint(*startPoint, possibleLoop[len(possibleLoop)-1]) {
+			fmt.Printf("Found loop of length %d\n", len(possibleLoop))
+			for _, point := range possibleLoop {
+				fmt.Printf("%s", point.symbol)
+			}
+			fmt.Println()
+			fmt.Println()
+			return possibleLoop
+		}
+	}
+	return nil
+}
+
+func ContinueDirection(inputFileName string, currentPath []MapPoint) []MapPoint {
+	startPoint := currentPath[0]
+	currentPoint := currentPath[len(currentPath)-1]
+
+	if IsStartingPoint(startPoint, currentPoint) {
+		return currentPath
+	}
+
+	validMoves := GetAdjacentValidMoves(inputFileName, currentPoint.x, currentPoint.y)
+	for _, validMove := range validMoves {
+
+		// avoid going back track
+		if IsPreviousPoint(currentPath, validMove) {
+			continue
+		}
+
+		currentPath = append(currentPath, validMove)
+		return ContinueDirection(inputFileName, currentPath)
+	}
+
+	return currentPath
+}
+
+func IsStartingPoint(startPoint MapPoint, currentPoint MapPoint) bool {
+	return startPoint.x == currentPoint.x && startPoint.y == currentPoint.y
+}
+
+func IsPreviousPoint(currentPath []MapPoint, currentPoint MapPoint) bool {
+	previousPoint := currentPath[len(currentPath)-2]
+	return previousPoint.x == currentPoint.x && previousPoint.y == currentPoint.y
+}
+
+func FindInsideObjectsWithinLoop(inputFileName string) int {
+	loopPoints := GetLoop(inputFileName)
+
+	inputData, err := os.ReadFile(inputFileName)
+	if err != nil {
+		fmt.Print(err)
+		return 0
+	}
+
+	objectsWithinLoop := 0
+	for x, dataLine := range strings.Split(string(inputData), "\n") {
+		for y, data := range dataLine {
+			firstPoint := GetNearestLoopPoint(MapPoint{x, y, string(data)}, loopPoints)
+			if firstPoint == nil {
+				break
+			}
+
+			// skip pipes
+			lastPointOnRow := GetLastLoopPoint(*firstPoint, loopPoints)
+			if lastPointOnRow == nil {
+				break
+			}
+			if IsHorizontalPipe(*firstPoint, *lastPointOnRow, loopPoints) {
+				break
+			}
+
+			secondPoint := GetNextLoopPoint(*firstPoint, loopPoints)
+			if secondPoint == nil {
+				break
+			}
+
+			// skip adjacent points
+			if firstPoint.y+1 == secondPoint.y {
+				break
+			}
+
+			// check if data is in between firstPoint and secondPoint
+			objectsWithinLoop++
+		}
+	}
+
+	return objectsWithinLoop
+}
+
+func GetNearestLoopPoint(currentPoint MapPoint, loopPoints []MapPoint) *MapPoint {
+	for _, loopPoint := range loopPoints {
+		if loopPoint.x != currentPoint.x {
+			continue
+		}
+		if loopPoint.y > currentPoint.y {
+			return &loopPoint
+		}
+	}
+	return nil
+}
+
+func GetLastLoopPoint(currentPoint MapPoint, loopPoints []MapPoint) *MapPoint {
+
+	farestPoint := MapPoint{currentPoint.x, currentPoint.y, currentPoint.symbol}
+	for _, loopPoint := range loopPoints {
+		if loopPoint.x != currentPoint.x {
+			continue
+		}
+		if loopPoint.y > farestPoint.y {
+			farestPoint = MapPoint{loopPoint.x, loopPoint.y, loopPoint.symbol}
+		}
+	}
+
+	if farestPoint.x == currentPoint.x && farestPoint.y == currentPoint.y {
+		return nil
+	}
+	return &farestPoint
+}
+
+func GetNextLoopPoint(point MapPoint, loopPoints []MapPoint) *MapPoint {
+
+	nearestY := MapPoint{0, math.MaxInt, "0"}
+	for _, possibleNearestPoint := range loopPoints {
+		if possibleNearestPoint.x != point.x {
+			continue
+		}
+		if possibleNearestPoint.y > point.y && possibleNearestPoint.y <= nearestY.y {
+			nearestY = MapPoint{possibleNearestPoint.x, possibleNearestPoint.y, possibleNearestPoint.symbol}
+		}
+	}
+
+	if nearestY.x == point.x && nearestY.y == point.y {
+		return nil
+	}
+	return &nearestY
+}
+
+func IsHorizontalPipe(leftCorner MapPoint, rightCorner MapPoint, loop []MapPoint) bool {
+	row := leftCorner.x
+
+	leftColumn := leftCorner.y
+	rightColumn := rightCorner.y
+
+	for i := leftColumn + 1; i < rightColumn; i++ {
+		if !slices.Contains(loop, MapPoint{row, i, "-"}) {
+			return false
+		}
+	}
+	return true
+}
---
+++ 2023/day10/challenge_test.go
@@ -0,0 +1,77 @@
+package challenge
+
+import (
+	"math"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("challenge", func() {
+	It("FindStart", func() {
+		mapPoint := FindStart("star_sample.txt")
+		Expect(mapPoint.x).To(Equal(1))
+		Expect(mapPoint.y).To(Equal(1))
+
+		// TODO more tests if required
+	})
+
+	// TODO test IsValidAdjacent id needed
+
+	It("GetAdjacentValidMoves", func() {
+		validMoves := GetAdjacentValidMoves("star_sample.txt", 1, 1)
+		Expect(validMoves).To(Equal([]MapPoint{MapPoint{2, 1, "|"}, MapPoint{1, 2, "-"}}))
+
+		// TODO more tests if required
+	})
+
+	It("GetsLoop", func() {
+		loop := GetLoop("star_sample.txt")
+		Expect(loop).To(Equal([]MapPoint{
+			{1, 1, "S"},
+			{2, 1, "|"},
+			{3, 1, "L"},
+			{3, 2, "-"},
+			{3, 3, "J"},
+			{2, 3, "|"},
+			{1, 3, "7"},
+			{1, 2, "-"},
+			{1, 1, "S"}}))
+
+		Expect(float64((len(loop)) / 2)).To(Equal(float64(4)))
+	})
+
+	Context("Star1", func() {
+		When("given sample", func() {
+			It("resolves", func() {
+				loop := GetLoop("star_sample2.txt")
+				Expect(math.Ceil(float64(len(loop) / 2))).To(Equal(float64(8)))
+			})
+		})
+		When("given input", func() {
+			It("resolves", func() {
+				loop := GetLoop("star_input.txt")
+				farestPoint := math.Ceil(float64(len(loop) / 2))
+				Expect(farestPoint).NotTo(Equal(float64(6883)))
+				Expect(farestPoint).NotTo(Equal(float64(6884)))
+				Expect(farestPoint).NotTo(Equal(float64(5119)))
+				Expect(farestPoint).NotTo(Equal(float64(6191)))
+				Expect(farestPoint).NotTo(Equal(float64(6192)))
+				Expect(farestPoint).NotTo(Equal(float64(6193)))
+				Expect(farestPoint).To(Equal(float64(6882)))
+			})
+		})
+	})
+	FContext("Star2", func() {
+		When("given sample", func() {
+			It("resolves", func() {
+				Expect(FindInsideObjectsWithinLoop("star2_sample1.txt")).To(Equal(4))
+			})
+		})
+		//XWhen("given input", func() {
+		//	It("resolves", func() {
+		//		Expect(Star1("star_input.txt")).To(Equal(0))
+		//	})
+		//})
+	})
+})
