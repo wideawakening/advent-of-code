263b24d85696a4baeb9c8c312e84bc03089968b3
iru <irune.prado@canonical.com>
2024-12-03 22:11:48 +0100

    don't you want me

--- 2024/day3/challenge.py
+++ 2024/day3/challenge.py
@@ -1,10 +1,9 @@
 import re
 
 def resolve_star1(file) -> int:
-    return calculate(read_file(file))
+    return calculate_star1(read_file(file))
 
-# todo? trim
-def calculate(content) -> int:
+def calculate_star1(content) -> int:
     total = 0
     pattern = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')  # Groups for area, prefix, and line number
 
@@ -17,8 +16,25 @@ def calculate(content) -> int:
 
 
 def resolve_star2(file) -> int:
-    result = 0
-    return result
+    return calculate_star2(read_file(file))
+
+
+def calculate_star2(content) -> int:
+    total = 0
+    on = True
+    pattern = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))")
+    for found in re.findall(pattern, content):
+        if "do()" in found[2]:
+            on = True
+        else:
+            if "don't()" in found[3]:
+                on = False
+            else:
+                if on:
+                    num1, num2 = found[0], int(found[1])
+                    print(num1, num2)
+                    total += int(num1) * int(num2)
+    return total
 
 
 def read_file (file_path):
