d195265bb056ecd4d7c18b38d89101df75509809
iru <irune.prado@canonical.com>
2024-12-05 19:57:35 +0100

    test: fix: assertion order

--- 2024/_template/challenge_test.py
+++ 2024/_template/challenge_test.py
@@ -6,19 +6,19 @@ class MyTestCase(unittest.TestCase):
 
     def test_star_1_sample(self):
         result: int = challenge.resolve_star1('star_sample.txt')
-        self.assertEqual(result, 0)
+        self.assertEqual(0, result)
 
     def test_star_1_input(self):
         result: int = challenge.resolve_star1('star_input.txt')
-        self.assertEqual(result, 0)
+        self.assertEqual(0, result)
 
     def test_star_2_sample(self):
         result: int = challenge.resolve_star2('star_sample.txt')
-        self.assertEqual(result, 0)
+        self.assertEqual(0, result)
 
     def test_star_2_input(self):
      result: int = challenge.resolve_star2('star_input.txt')
-     self.assertEqual(result, 0)
+     self.assertEqual(0, result)
 
 if __name__ == '__main__':
     unittest.main()
