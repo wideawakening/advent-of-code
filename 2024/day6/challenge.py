5241729c4f06cf49518c39059f400f2bcc84363c
iru <irune.prado@canonical.com>
2024-12-07 22:42:57 +0100

    day6-part1 fixed!

--- 2024/day6/challenge.py
+++ 2024/day6/challenge.py
@@ -1,9 +1,80 @@
 
 def resolve_star1(file) -> int:
-    read_file(file)
+    map = read_file(file)
 
-    result = 0
-    return result
+    current_figure = '^'
+    current_position_tuple = map[current_figure]
+    current_position = [current_position_tuple[0][0], current_position_tuple[0][1]]  #row, col
+    map['X'] = []
+    map['X'].append([current_position[0], current_position[1]])
+    current_position[0] = current_position[0] - 1
+    map['X'].append([current_position[0], current_position[1]])
+
+
+    print()
+    while not is_exit(current_figure, current_position):
+
+        if is_next_position_blocked(current_position, current_figure, map):
+            match current_figure:
+                case '^':
+                    current_figure = '>'
+                    print('changed direction to >')
+                case 'v':
+                    current_figure = '<'
+                    print('changed direction to <')
+                case '>':
+                    current_figure = 'v'
+                    print('changed direction to v')
+                case '<':
+                    current_figure = '^'
+                    print('changed direction to ^')
+
+        match current_figure:
+            case '^':
+                current_position[0] = current_position[0] - 1
+            case 'v':
+                current_position[0] = current_position[0] + 1
+            case '>':
+                current_position[1] = current_position[1] + 1
+            case '<':
+                current_position[1] = current_position[1] - 1
+
+        if current_position not in map['X']:
+            map['X'].append([current_position[0], current_position[1]])
+            print(current_position)
+
+    print()
+    print(map['X'])
+    return len(map['X'])
+
+def is_next_position_blocked(current_position, current_figure, map) -> bool:
+    next_position = ()
+    match current_figure:
+        case '^':
+            next_position = (current_position[0] - 1, current_position[1])
+        case 'v':
+            next_position = (current_position[0] + 1, current_position[1])
+        case '>':
+            next_position = (current_position[0], current_position[1] + 1)
+        case '<':
+            next_position = (current_position[0], current_position[1] - 1)
+    return next_position in map['#']
+
+cols = 0
+rows = 0
+def is_exit(current_figure: str, current_position: []) -> bool:
+    match current_figure:
+        case '^':
+            return current_position[0] == 0
+        case 'v':
+            return current_position[0] == rows
+        case '>':
+            return current_position[1] == cols
+        case '<':
+            return current_position[1] == 0
+    return False
+
+
 
 def resolve_star2(file) -> int:
     read_file(file)
@@ -12,12 +83,17 @@ def resolve_star2(file) -> int:
     return result
 
 
+
 def read_file (file):
-    column1, column2 = [], []
-    with open(file) as data_file:
-        for data in data_file:
-            num1, num2 = map(int, data.split())
-            column1.append(num1)
-            column2.append(num2)
-    return column1, column2
+    print()
+    map = {}
+    with open(file, 'r') as file:
+        for row_index, line in enumerate(file):
+            for col_index, str in enumerate(line.strip()):
+                if str not in map:
+                    map[str] = []
+                map[str].append((row_index, col_index))
+    # for str, pos in map.items():
+    #     print(f"'{str}': {pos}")
+    return map
 
--- 2024/day6/challenge_test.py
+++ 2024/day6/challenge_test.py
@@ -5,16 +5,25 @@ import challenge
 class MyTestCase(unittest.TestCase):
 
     def test_star_1_sample(self):
+        challenge.rows = 9
+        challenge.cols = 10
         result: int = challenge.resolve_star1('star_sample.txt')
-        self.assertEqual(0, result)
+        self.assertEqual(41, result)
 
     def test_star_1_input(self):
+        challenge.rows = 129
+        challenge.cols = 130
         result: int = challenge.resolve_star1('star_input.txt')
+        self.assertNotEqual(5411, result)
+        self.assertNotEqual(5067, result)
+        self.assertNotEqual(985, result)
+
         self.assertEqual(0, result)
+
 
     def test_star_2_sample(self):
         result: int = challenge.resolve_star2('star_sample.txt')
-        self.assertEqual(0, result)
+        self.assertEqual(6, result)
 
     def test_star_2_input(self):
      result: int = challenge.resolve_star2('star_input.txt')
