33758f6f863ef547fc00162f6c7aeacdb878f750
iru <irune.prado@canonical.com>
2024-12-07 22:08:54 +0100

    day7-part2

--- 2024/day7/challenge.py
+++ 2024/day7/challenge.py
@@ -1,36 +1,36 @@
+
 def resolve_star1(file) -> int:
     operations = read_file(file)
-
     result = 0
     for operation_result in operations:
         if has_possible_operation(operation_result, operations[operation_result]):
             result += operation_result
+            print("valid: ", operation_result)
     return result
 
 def has_possible_operation(operation_result:int, operation_values:list[int]) -> bool:
 
-    def operate(current_result: int, operation_result:int, operation_values:list[int]) -> bool:
-        if current_result == operation_result:
+    def operate(current_result: int, operation_result_internal:int, operation_values_internal:list[int]) -> bool:
+
+        if current_result == operation_result_internal and len(operation_values_internal)==0:
             return True
 
-        if len(operation_values) == 0:
+        if len(operation_values_internal) == 0:
             return False
 
-        if len(operation_values) == 1 and operation_result == operation_values[0]:
+        if len(operation_values_internal) == 1 and operation_result_internal == operation_values_internal[0]:
             return True
 
         return (
-            operate(current_result + operation_values[0], operation_result, operation_values[1:]) or
-            operate(current_result * operation_values[0], operation_result, operation_values[1:])
+                operate(current_result + operation_values_internal[0], operation_result_internal, operation_values_internal[1:]) or
+                operate(current_result * operation_values_internal[0], operation_result_internal, operation_values_internal[1:]) or
+                operate(int(str(current_result) + str(operation_values_internal[0])), operation_result_internal, operation_values_internal[1:])
             )
 
     return operate(0, operation_result, operation_values)
 
 def resolve_star2(file) -> int:
-    read_file(file)
-
-    result = 0
-    return result
+   return resolve_star1(file)
 
 
 
--- 2024/day7/challenge_test.py
+++ 2024/day7/challenge_test.py
@@ -1,5 +1,6 @@
 import unittest
 import challenge
+from day7.challenge import has_possible_operation
 
 
 class MyTestCase(unittest.TestCase):
@@ -14,10 +15,18 @@ class MyTestCase(unittest.TestCase):
 
     def test_star_2_sample(self):
         result: int = challenge.resolve_star2('star_sample.txt')
-        self.assertEqual(0, result)
+        self.assertEqual(11387, result)
+
+    def test_star_2_samples(self):
+        self.assertEqual(False, has_possible_operation(100, [50,50,2]))
 
     def test_star_2_input(self):
      result: int = challenge.resolve_star2('star_input.txt')
+     self.assertNotEqual(264184082159178, result) # too high #concat_numbers # int(str(),str())
+     self.assertNotEqual(3122337891434, result) #  current_result*10^math.ceil(math.log(operation_value, 10))+operation_value
+     self.assertNotEqual(3119477785044, result)
+     self.assertNotEqual(3122183584666, result)  # too low
+
      self.assertEqual(0, result)
 
 if __name__ == '__main__':
