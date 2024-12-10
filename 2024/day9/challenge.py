c0c4b9c3a9d234ee966bb3f5b6ab4435e2fa6072
iru <irune.prado@canonical.com>
2024-12-10 21:06:21 +0100

    day9,part1-ok!

--- 2024/day9/challenge.py
+++ 2024/day9/challenge.py
@@ -1,12 +1,42 @@
-import re
-from io import StringIO
-
-# disk map defrag
-# defrag to free space, one at a time, from the leftmost free space block (until there are no remaining gaps)
 def resolve_star1(file) -> int:
-    unfrag_data = convert_to_pre_defrag(read_file(file))
-    defrag_data = defrag(unfrag_data)
-    return get_checksum(defrag_data)
+    unfrag_data = list_convert_to_pre_defrag(read_file(file))
+    defrag_data = list_defrag(unfrag_data)
+    return list_get_checksum(defrag_data)
+
+def list_convert_to_pre_defrag(data:str)->[]:
+    defrag_data = []
+    id = 0
+    file_id = 0
+    for c in data:
+        if id==0 or id%2==0:
+            for free_space in range(int(c)):
+                defrag_data.append(file_id)
+            file_id += 1
+        else:
+            if id%2 != 0:
+                for free_space in range(int(c)):
+                    defrag_data.append(".")
+        id += 1
+
+    return defrag_data
+
+def list_defrag(defrag_data:list)->[str]:
+    while '.' in defrag_data:
+        idx_space = defrag_data.index(".")
+        defrag_data[idx_space] = defrag_data[len(defrag_data)-1]
+        defrag_data.pop()
+
+    return defrag_data
+
+def list_get_checksum(data:list) -> int:
+    total = 0
+    id = 0
+
+    for num in data:
+        total += int(num) * id
+        id += 1
+    return total
+
 
 def resolve_star2(file) -> int:
     read_file(file)
@@ -14,58 +44,110 @@ def resolve_star2(file) -> int:
     result = 0
     return result
 
-def convert_to_pre_defrag(data:str)->str:
-    buffer = StringIO()
-    id = 0
-    file_id = 0
-    for c in data:
-        if id==0 or id%2==0:
-            for free_space in range(int(c)):
-                buffer.write(str(file_id))
-            file_id += 1
-        else:
-            if id%2 != 0:
-                for free_space in range(int(c)):
-                    buffer.write(".")
-        id += 1
-
-    return buffer.getvalue()
-
-def defrag(data:str) ->str:
-    defrag_data = list(data)
-
-    while not bool(re.fullmatch(r"^\d*\.*$", "".join(map(str, defrag_data)))):
-        idx_space = "".join(map(str, defrag_data)).index(".")
-        match = re.search(r"[^.](?=\.*$)", "".join(map(str, defrag_data)))
-        if match:
-            idx_number =  match.start()
-            val1 = defrag_data[idx_space]
-            val2 = defrag_data[idx_number]
-            defrag_data[idx_space] = val2
-            defrag_data[idx_number] = val1
-            # print("".join(map(str, defrag_data)))
-        else:
-            break
-
-    # optimization
-    result = "".join(map(str, defrag_data))
-    result = result.replace(".","")
-    print(result)
-    return result
-
-
-
-def get_checksum(data:str) -> int:
-    total = 0
-    id = 0
-    for c in data:
-        if c == ".":
-            break
-        total += int(c)*id
-        id += 1
-    return total
-
 def read_file (file):
     with open(file) as file:
         return file.read()
 
+
+# failed ideas once you realize IDs have more than one digit
+
+# def ko_convert_to_pre_defrag(data:str)->str:
+#     buffer = StringIO()
+#     id = 0
+#     file_id = 0
+#     for c in data:
+#         if id==0 or id%2==0:
+#             for free_space in range(int(c)):
+#                 buffer.write(str(file_id))
+#             file_id += 1
+#         else:
+#             if id%2 != 0:
+#                 for free_space in range(int(c)):
+#                     buffer.write(".")
+#         id += 1
+#
+#     return buffer.getvalue()
+#
+# def convert_to_pre_defrag(data:str)->str:
+#     buffer = StringIO()
+#     id = 0
+#     file_id = 0
+#     for c in data:
+#         if id==0 or id%2==0:
+#             for free_space in range(int(c)):
+#                 buffer.write(f'({str(file_id)})')
+#             file_id += 1
+#         else:
+#             if id%2 != 0:
+#                 for free_space in range(int(c)):
+#                     buffer.write(".")
+#         id += 1
+#
+#     return buffer.getvalue()
+#
+#
+#
+# def ko_defrag(data:str) ->str:
+#     defrag_data = list(data)
+#
+#     while not bool(re.fullmatch(r"^\d*\.*$", "".join(map(str, defrag_data)))):
+#         idx_space = "".join(map(str, defrag_data)).index(".")
+#         match = re.search(r"[^.](?=\.*$)", "".join(map(str, defrag_data)))
+#         if match:
+#             idx_number =  match.start()
+#             val1 = defrag_data[idx_space]
+#             val2 = defrag_data[idx_number]
+#             defrag_data[idx_space] = val2
+#             defrag_data[idx_number] = val1
+#             # print("".join(map(str, defrag_data)))
+#         else:
+#             break
+#
+#     # optimization
+#     result = "".join(map(str, defrag_data))
+#     result = result.replace(".","")
+#     print(result)
+#     return result
+#
+#
+# def get_checksum(data:str) -> int:
+#     total = 0
+#     id = 0
+#
+#     match = re.findall(r"\((\d*)\)", data)
+#     for num in match:
+#         total += int(num) * id
+#         id += 1
+#     return total
+#
+# def ko_get_checksum(data:str) -> int:
+#     total = 0
+#     id = 0
+#     for c in data:
+#         if c == ".":
+#             break
+#         total += int(c)*id
+#         id += 1
+#     return total
+
+## find first '.' manually, no regexp, no strings
+# def defrag(data:str) ->str:
+#     defrag_data = list(data)
+#
+#     # while not bool(re.fullmatch(r"^\(\d*\)\.*$", data)):
+#     #     data = "".join(map(str, defrag_data))
+#     #     idx_space = data.index(".")
+#     #     match = re.search(r"\((\d)\)$", data)
+#         # if match:
+#         #     number = match.group(1)
+#         #     val1 = defrag_data[idx_space]
+#         #     # defrag_data[idx_space] = val2
+#         #     defrag_data[idx_number] = val1
+#         #     print(data)
+#         #     data = data.rstrip(".")
+#         # else:
+#         #     break
+#
+#     # optimization
+#     print(data)
+#     return data
--- 2024/day9/challenge_test.py
+++ 2024/day9/challenge_test.py
@@ -5,10 +5,17 @@ import challenge
 class MyTestCase(unittest.TestCase):
 
     def test_start_1_snippet(self):
+        self.assertEqual([0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6], challenge.list_defrag([0, 0, '.', '.', '.', 1, 1, 1, '.', '.', '.', 2, '.', '.', '.', 3, 3, 3, '.', 4, 4, '.', 5, 5, 5, 5, '.', 6, 6, 6, 6, '.', 7, 7, 7, '.', 8, 8, 8, 8, 9, 9]))
+
+
         self.assertEqual("00...111...2...333.44.5555.6666.777.888899",
+                         challenge.ko_convert_to_pre_defrag(challenge.read_file('star_sample.txt')))
+        self.assertEqual("(0)(0)...(1)(1)(1)...(2)...(3)(3)(3).(4)(4).(5)(5)(5)(5).(6)(6)(6)(6).(7)(7)(7).(8)(8)(8)(8)(9)(9)",
                          challenge.convert_to_pre_defrag(challenge.read_file('star_sample.txt')))
 
-        self.assertEqual(1928, challenge.get_checksum('0099811188827773336446555566..............'))
+        self.assertEqual(1928, challenge.ko_get_checksum('0099811188827773336446555566..............'))
+        self.assertEqual(1928, challenge.list_get_checksum([0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6]))
+
 
     def test_star_1_sample(self):
         result: int = challenge.resolve_star1('star_sample.txt')
